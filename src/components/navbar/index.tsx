34e7ac330a99c29ca6e42d015c8dd6ba7ec1aec8
Hamza Arif khan <HamzaArifkhan70@gmail.com>
2024-11-13 11:56:04 +0500

    first push

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,6 @@
 import { FaGithub } from "react-icons/fa";
 import { TiSocialLinkedinCircular } from "react-icons/ti";
-import { CiLight } from "react-icons/ci";
+// import { CiLight } from "react-icons/ci";
 import { useState } from "react";
 import { IoSunny } from "react-icons/io5";
 import { IoMoon } from "react-icons/io5";
@@ -12,7 +12,7 @@ const Navbar = () => {
     document.body.classList.toggle("dark");
   };
   return (
-    <div className="flex justify-between px-32 py-8">
+    <div className="flex justify-between px-32 py-8 bg-yellow-100 text-black dark:bg-blue-900 dark:text-white">
       <ul className="flex space-x-5">
         <li>Home</li>
         <li>About</li>
@@ -26,7 +26,7 @@ const Navbar = () => {
         <li className="text-4xl">
           <TiSocialLinkedinCircular />
         </li>
-        <li className="text-3xl">
+        <li className="text-3xl mt-1 ">
           <button onClick={() => darkModeHandler()}>
             {
               dark && <IoSunny /> // render sunny when dark is true
