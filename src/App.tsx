b1f4e8ec667ccc76887ab6cd8b5ab16ee03b3ecd
Hamza Arif khan <HamzaArifkhan70@gmail.com>
2024-11-20 19:04:30 +0500

    94 setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
-function App() {
-   
-   const [dark, setDark] = useState(true);
 
-   useEffect(() => {
-     const savedTheme = localStorage.getItem('theme');
-     if (savedTheme === 'dark') {
-       setDark(true);
-       document.body.classList.add('dark'); // Set dark mode globally
-     } else {
-       setDark(false);
-       document.body.classList.remove('dark'); // Set light mode globally
-     }
-   }, []);
-   const darkModeHandler = () => {
+function App() {
+  const [dark, setDark] = useState(true);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      setDark(true);
+      document.body.classList.add("dark"); // Set dark mode globally
+    } else {
+      setDark(false);
+      document.body.classList.remove("dark"); // Set light mode globally
+    }
+  }, []);
+  const darkModeHandler = () => {
     setDark(!dark);
-    const newTheme = !dark ? 'dark' : 'light';
-    localStorage.setItem('theme', newTheme); // Save theme preference to localStorage
-    document.body.classList.toggle('dark'); // Toggle dark mode globally
+    const newTheme = !dark ? "dark" : "light";
+    localStorage.setItem("theme", newTheme); // Save theme preference to localStorage
+    document.body.classList.toggle("dark"); // Toggle dark mode globally
   };
   return (
-    <>
-     <Navbar dark={dark} darkModeHandler={darkModeHandler} />
-    </>
+    <div className="px-32 py-8">
+      <Navbar dark={dark} darkModeHandler={darkModeHandler} />
+    </div>
   );
 }
 
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,15 +2,15 @@ import { FaGithub } from "react-icons/fa";
 import { TiSocialLinkedinCircular } from "react-icons/ti";
 import { IoSunny } from "react-icons/io5";
 import { IoMoon } from "react-icons/io5";
-import './style.css';
+import "./style.css";
 interface NavbarProps {
   dark: boolean;
   darkModeHandler: () => void;
 }
-const Navbar = ({ dark, darkModeHandler }:NavbarProps) => {
+const Navbar = ({ dark, darkModeHandler }: NavbarProps) => {
   return (
     // <div className="flex justify-between px-32 py-8 bg-yellow-100 text-black dark:bg-blue-900 dark:text-white">
-    <div className="flex justify-between px-32 py-8">
+    <div className="flex justify-between ">
       <ul className="flex space-x-5">
         <li>Home</li>
         <li>About</li>
@@ -27,7 +27,7 @@ const Navbar = ({ dark, darkModeHandler }:NavbarProps) => {
         <li className="text-2xl mt-1 ">
           <button onClick={darkModeHandler}>
             {dark ? (
-              <IoSunny className={`${dark ? 'rotate-sun' : ''}`} />
+              <IoSunny className={`${dark ? "rotate-sun" : ""}`} />
             ) : (
               <IoMoon />
             )}
